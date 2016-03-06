866849438607c388e8954ec3024ae79d64e9e062
Alex Jordan <alex@strugee.net>
2016-03-06 02:21:13 -0800

    Start writing tests

---
+++ test/lib/start-app.js
@@ -0,0 +1,31 @@
+/*
+
+ Copyright 2015 Alex Jordan <alex@strugee.net>.
+
+ This file is part of microjourn.al.
+
+ microjourn.al is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ microjourn.al is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with microjourn.al.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+// test/lib/start-app.js - set up the app for testing
+
+var app = require('../../app.js');
+
+function start(cb) {
+	var server = app.listen(52472, 'localhost');
+	return server;
+}
+
+module.exports = start;
