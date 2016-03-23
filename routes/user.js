458f178df18c1d3aea75d53900171f916ba5d17c
Alex Jordan <alex@strugee.net>
2016-03-23 16:37:44 -0700

    Scaffold out the /user route

--- routes/user.js
+++ routes/user.js
@@ -20,77 +20,21 @@
 
  */
 
-// routes/moment.js - routing handler for /moment
+// routes/user.js - routing handler for /user
 
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/mongo');
-var Moment = require('../lib/moment');
 
 router.get('/', function(req, res) {
-	res.send('Got a GET request for /moment/');
+	res.send('Got a GET request for /user/');
 });
 
 router.get('/:id', function(req, res) {
-	// TODO
 
-	// db.get('SELECT * FROM moments WHERE id = ?;', req.params.id, function(err, row) {
-	// 	if (err) {
-	// 		res.writeHead(500);
-	// 		res.end(err.toString(), function() {
-	// 			throw err;
-	// 		});
-	// 	} else if (row === undefined) {
-	// 		res.writeHead(404);
-	// 		res.end();
-	// 	} else {
-	// 		res.writeHead(200);
-	// 		res.write('{"active":');
-	// 		if (row['active'] === 0) {
-	// 			res.write('true');
-	// 		} else {
-	// 			res.write('false');
-	// 		}
-	// 		res.write(',"name":"' + row['name'] + '"}');
-	// 		res.end();
-	// 	}
-	// });
 });
 
 router.post('/', function(req, res, params) {
-	// sanity checks
-
-	if (!req.body.timestamp) {
-		res.setHeader('Content-Type', 'text/plain');
-		res.writeHead(400);
-		res.end('Expected a "timestamp" key.');
-		return;
-	}
-
-	if (!req.body.content) {
-		res.setHeader('Content-Type', 'text/plain');
-		res.writeHead(400);
-		res.end('Expected a "content" key.');
-		return;
-	}
-
-	var moment = new Moment({
-		timestamp: req.body.timestamp,
-		content: req.body.content
-	});
-
-	moment.save(function(err) {
-		if (err) {
-			res.writeHead(500);
-			res.end(err.toString(), function() {
-				throw err;
-			});
-			return;
-		}
-
-		res.writeHead(201);
-		res.end('{"id":' + 'unknown' + '}');
-	});
 });
 
 module.exports = router;
