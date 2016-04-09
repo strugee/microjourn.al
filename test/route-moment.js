9e2daae8cad1ed9eaabb0fcf9496bf061120d9fd
Alex Jordan <alex@strugee.net>
2016-04-09 18:18:06 -0400

    Write some tests
    These are mostly failing right now.

--- test/route-moment.js
+++ test/route-moment.js
@@ -26,6 +26,9 @@ var http = require('http');
 var request = require('superagent');
 var startApp = require('./lib/start-app.js');
 
+var postResponse;
+var momentId;
+
 var server;
 
 module.exports = {
@@ -37,7 +40,7 @@ module.exports = {
 		server.close();
 	},
 
-	'/moment route': {
+	'/moments route': {
 		'unauthorized request rejection': {
 			'should return the proper status code': function(done) {
 				request.get('http://localhost:52472/moments/').end(function(err, res) {
@@ -60,37 +63,83 @@ module.exports = {
 		},
 		'GET on a nonexistant resource': {
 			'should return the proper status code': function(done) {
-				request.get('http://localhost:52472/moments/', function(err, res) {
-					assert.equal(res.statusCode, 401);
-					done();
-				});
+				request.get('http://localhost:52472/moments/nonexistant')
+				       .set('Authorization', 'token foobar')
+				       .end(function(err, res) {
+				        	assert.equal(res.statusCode, 404);
+				        	done();
+				       });
 			},
 			'should return the proper Content-Type': function(done) {
-				request.get('http://localhost:52472/moments/', function(err, res) {
-					assert.equal(res.headers['content-type'], 'text/plain');
-					done();
-				});
+				request.get('http://localhost:52472/moments/nonexistant')
+				       .set('Authorization', 'token foobar')
+				       .end(function(err, res) {
+				        	assert.equal(res.headers['content-type'], 'text/html; charset=utf-8');
+				        	done();
+				       });
 			}
 		},
 		'POST to /moments/ without a timestamp key': {
 			'should return the proper status code': null,
-			'should return the proper Content-Type': null,
+			'should return the proper Content-Type': function(done) {
+				request.post('http://localhost:52472/moments/')
+				       .set('Authorization', 'token foobar')
+				       .send('{content: "Hello World from MochaJS!"}')
+				       .end(function(err, res) {
+				        	assert.equal(res.headers['content-type'], 'text/plain');
+				        	done();
+				       });
+			},
 			'should include the proper response body message': null
 		},
 		'POST to /moments/ without a content key': {
 			'should return the proper status code': null,
-			'should return the proper Content-Type': null,
+			'should return the proper Content-Type': function(done) {
+				request.post('http://localhost:52472/moments/')
+				       .set('Authorization', 'token foobar')
+				       .send('{timestamp: 123456789}')
+				       .end(function(err, res) {
+				        	assert.equal(res.headers['content-type'], 'text/plain');
+				        	done();
+				       });
+
+			},
 			'should include the proper response body message': null
 		},
 		'POST to /moments/': {
-			'should return the proper status code': null,
-			'should return the proper Content-Type': null,
-			'should return parseable JSON in the body': null,
-			'should include the id in the body': null
+			'should return the proper status code': function(done) {
+				request.post('http://localhost:52472/moments/')
+				       .set('Authorization', 'token foobar')
+				       .send('{content: "Hello World from MochaJS!", timestamp: 123456789}')
+				       .end(function(err, res) {
+				        	postResponse = res;
+				        	assert.equal(res.headers['content-type'], 'text/plain');
+				        	done();
+				       });
+			},
+			'should return the proper Content-Type': function(done) {
+				assert.equal(postResponse.headers['content-type'], 'text/plain');
+			},
+			'should return parseable JSON in the body': function(done) {
+				assert.doesNotThrow(function() {
+					JSON.parse(postResponse.text);
+				}, SyntaxError);
+			},
+			'should include the id in the body': function(done) {
+				assert.ok("id" in postResponse.body);
+				momentId = postResponse.body.id;
+			}
 		},
 		'GET from /moments/:id': {
 			'should return the proper status code': null,
-			'should return the proper Content-Type': null,
+			'should return the proper Content-Type': function(done) {
+				request.get('http://localhost:52472/moments/' + momentId)
+				       .set('Authorization', 'token foobar')
+				       .end(function(err, res) {
+				        	assert.equal(res.headers['content-type'], 'text/json');
+				        	done();
+				       });
+			},
 			'should return parseable JSON in the body': null,
 			'should include the id in the body': null,
 			'should include the content in the body': null,
@@ -98,7 +147,14 @@ module.exports = {
 		},
 		'HEAD from /moments/:id': {
 			'should return the proper status code': null,
-			'should return the proper Content-Type': null,
+			'should return the proper Content-Type': function(done) {
+				request.get('http://localhost:52472/moments/' + momentId)
+				       .set('Authorization', 'token foobar')
+				       .end(function(err, res) {
+				        	assert.equal(res.headers['content-type'], 'text/json');
+				        	done();
+				       });
+			},
 			'should return parseable JSON in the body': null,
 			'should return an empty body': null
 		},
@@ -139,7 +195,7 @@ module.exports = {
 		'GET to /moments/:id after DELETE': {
 			'should return the proper status code': null
 		},
-		'PUT to a non-existant /moments/:id': {
+		'PUT to a nonexistant /moments/:id': {
 			'should return the proper status code': null,
 			'should return the proper Content-Type': null,
 			'should return the proper response body message': null
